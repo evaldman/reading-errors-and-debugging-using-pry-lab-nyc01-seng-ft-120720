255d9b30c9b8079274095e4e84bb7fea1fadacb2
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 00:19:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,8 +8,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 def engage
-  puts state_log(date)
-  date = state_log
+  state_log(date)
+  date = generate_star_date
 end
 
 puts engage
