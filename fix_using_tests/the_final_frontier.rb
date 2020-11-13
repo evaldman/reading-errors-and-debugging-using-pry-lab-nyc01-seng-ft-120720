9d3d1d4746d659b27ef3c233f553f6b50c4890ad
charming-invocation-5395 <evaldman@yahoo.com>
2020-11-13 00:16:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -8,7 +8,6 @@ def state_log(star_date)
   star_date = generate_star_date
   "Captain's Log, star date #{star_date}."
 end
-binding.pry
 def engage
   puts state_log(date)
   date = generate_star_date
